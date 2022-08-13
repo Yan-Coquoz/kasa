532ffe3ded7ec2fff08baa90fbadd5019a42cc8d
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-08-13 17:54:31 +0200

    style sur le footer pour la page 404

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -8,7 +8,6 @@ import Loader from "../Loader";
 
 import "./style.scss";
 
-// const URLLocations ="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json";
 const jsonDatas = "/datas.json";
 
 const Location = () => {
